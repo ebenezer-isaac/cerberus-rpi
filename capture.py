35b209d81b98b18cacee68c7187a9299557079ae
ebenezerv99 <ebenezerv99@gmail.com>
2019-09-01 20:32:15 +0530

    added while loop

--- capture.py
+++ capture.py
@@ -8,19 +8,20 @@ f.Open(extra_info = True, check_baudrate = True)
 f.ChangeBaudrate(115200)
 print 'Place the finger on the scanner to capture finger'
 f.CmosLed(True)
-if f.IsPressFinger()[0]["Parameter"]=="""True""":
-	response = f.CaptureFinger()
-	if response[0]['ACK']:
-		print 'Image has been captured'
-		print 'Fetching image'
-		raw_img = f.GetImage()
-		print 'Image Fetched'
-		f.CmosLed(False)
-		f.Close()
-		print 'Closing connection...'
-		with open('capture.pickle', 'wb') as f:
-			pickle.dump(raw_img, f)
-	if response[0]['Parameter'] != 'NACK_FINGER_IS_NOT_PRESSED':
-		print 'Unknown Error occured', response[0]['Parameter']
+while f.IsPressFinger()[0]["Parameter"]=="""False""":
+	pass
+response = f.CaptureFinger()
+if response[0]['ACK']:
+	print 'Image has been captured'
+	print 'Fetching image'
+	raw_img = f.GetImage()
+	print 'Image Fetched'
+	f.CmosLed(False)
+	f.Close()
+	print 'Closing connection...'
+	with open('capture.pickle', 'wb') as f:
+		pickle.dump(raw_img, f)
+if response[0]['Parameter'] != 'NACK_FINGER_IS_NOT_PRESSED':
+	print 'Unknown Error occured', response[0]['Parameter']
 f.CmosLed(False)
 
