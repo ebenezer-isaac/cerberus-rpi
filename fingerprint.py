162acfecf86dcb8ec15132da7b0546f43916eb3f
ebenezerv99 <ebenezerv99@gmail.com>
2019-09-01 22:21:02 +0530

    implemented more functions

--- fingerprint.py
+++ fingerprint.py
@@ -1,19 +1,26 @@
 import fingerpi as fp
+f = fp.FingerPi()
 
 def Initialize_FPS():
-	f = fp.FingerPi()
-	response=f.Open(extra_info = True, check_baudrate = True)
-	print response
-
+	response =f.Open(extra_info = True, check_baudrate = True)
+	return response[0]['ACK']
+def SetLED_FPS(state):
+	response = f.CmosLed(state)
+	return response[0]['ACK']
 def Terminate_FPS():
-		response = f.Close()
-		print response
-		
-Initialize_FPS()
-Terminate_FPS()
-#CmosLed
-#ChangeBaudrate
-#GetEnrollCount
+	response = f.Close()
+	return response[0]['ACK']
+def SetBaudrate_FPS(baud):
+	response = f.ChangeBaudrate(int(baud))
+	return response[0]['ACK']
+def CountEnrolled_FPS():
+	print f.GetEnrollCount()
+print Initialize_FPS()
+print SetBaudrate_FPS(115200)
+print SetLED_FPS(True)
+print SetLED_FPS(False)
+CountEnrolled_FPS()
+print Terminate_FPS()
 #CheckEnrolled
 #EnrollStart
 #Enroll1
