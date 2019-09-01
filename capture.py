a3783df4ac33588398560d9c570568d16b4330dc
ebenezerv99 <ebenezerv99@gmail.com>
2019-09-01 20:41:42 +0530

    changed while condition to true

--- capture.py
+++ capture.py
@@ -9,7 +9,9 @@ f.ChangeBaudrate(115200)
 print 'Place the finger on the scanner to capture finger'
 f.CmosLed(True)
 fingerflag=f.IsPressFinger()[0]["Parameter"]
-while fingerflag=="""False""":
+while fingerflag!="""True""":
+	if fingerflag != 'NACK_FINGER_IS_NOT_PRESSED':
+		print 'Unknown Error occured', fingerflag
 	fingerflag=f.IsPressFinger()[0]["Parameter"]
 capture = f.CaptureFinger()
 if capture[0]['ACK']:
@@ -22,6 +24,5 @@ if capture[0]['ACK']:
 	print 'Closing connection...'
 	with open('capture.pickle', 'wb') as f:
 		pickle.dump(raw_img, f)
-if response[0]['Parameter'] != 'NACK_FINGER_IS_NOT_PRESSED':
-	print 'Unknown Error occured', response[0]['Parameter']
+
 
