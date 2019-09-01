8597744acfaa5ac0925d0f21c3f1fe5773845df5
ebenezerv99 <ebenezerv99@gmail.com>
2019-09-01 20:52:16 +0530

    minor syntax error

--- capture.py
+++ capture.py
@@ -9,17 +9,15 @@ f.ChangeBaudrate(115200)
 f.CmosLed(True)
 print 'Place the finger on the scanner to capture finger'
 fingerflag=f.IsPressFinger()[0]["Parameter"]
-while fingerflag!="""True""":
-	if fingerflag != '4114':
-		print 'Unknown Error occured', fingerflag
+while fingerflag==4114:
 	fingerflag=f.IsPressFinger()[0]["Parameter"]
 capture = f.CaptureFinger()
+f.CmosLed(False)
 if capture[0]['ACK']:
 	print 'Image has been captured'
 	print 'Fetching image'
 	raw_img = f.GetImage()
 	print 'Image Fetched'
-	f.CmosLed(False)
 	f.Close()
 	print 'Closing connection...'
 	with open('capture.pickle', 'wb') as f:
