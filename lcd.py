93c8cd0487dca148ec7932b4e6b522656baa3819
ebenezerv99 <ebenezerv99@gmail.com>
2019-09-03 21:41:30 +0530

    added lcd library

---
+++ lcd.py
@@ -0,0 +1,7 @@
+import lcd as lcddriver
+
+lcd = lcddriver.lcd()
+lcd.lcd_display_string("Hello world", 1)
+lcd.lcd_display_string("Ebenezer Isaac", 2)
+lcd.lcd_display_string("!@#$%^&*()", 3)
+lcd.lcd_display_string("I am a Raspberry 1.3", 4)
