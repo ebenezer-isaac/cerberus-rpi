41378a92888f498c5c30099046308bcdf054927e
ebenezerv99 <ebenezerv99@gmail.com>
2019-09-05 18:25:25 +0530

    testing database template

---
+++ print_enrolled.py
@@ -0,0 +1,13 @@
+from driver_fps import *
+Initialize_FPS()
+count = CountEnrolled_FPS()
+i = 0;
+print 'Total number of enrolled fingerprints = '+str(count)
+found=0
+while (found<count):
+	if CheckEnrolled_FPS(i):
+		print i
+		found = found+1
+	i=i+1
+print
+Terminate_FPS()
