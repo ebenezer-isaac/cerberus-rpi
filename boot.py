fdc5d1e84d5646dc867abee2eaf09ca49374e532
Ebenezer Isaac <ebenezerv99@gmail.com>
2019-10-05 14:30:57 +0530

    enroll

--- boot.py
+++ boot.py
@@ -1,85 +1,25 @@
 #To run this python script on startup (ignore quotes)
 #Run 'crontab -e'
-#Paste '@reboot sh /home/pi/cerberus-rpi/launcher.sh >/home/pi/bootlog/cronlog 2>&1' at the end of the file
-import mysql.connector, subprocess, time, json, os
-from drivers.lcd import LCD
-from drivers.rtc import RTC
-from drivers.fingerpi import FingerPi
-import datetime
-import calendar
+#Paste '@reboot python /home/pi/cerberus-rpi/boot.py >/home/pi/bootlog/cronlog 2>&1' at the end of the file
 import functions
-import time
-import RPi.GPIO as GPIO
-lcd = LCD()
-rtc = RTC()
-fps = FingerPi()
-def print_time():
-	lcd.clrscr()
-	lcd.println("Time :")
-        x = rtc.getTime()
-        t = time.time()
-        print 'Printing time on LCD'
-        print 'Press Ctrl + C to Abort'
-	t = time.time()
-	tx = time.time()
-        while tx-t<5:
-                y = rtc.getTime()
-                if not x==y:
-                        lcd.println(str(rtc.hour)+':'+str(rtc.min)+':'+str(rtc.sec)+' '+str(rtc.date)+'/'+str(rtc.month)+'/'+str(rtc.year),3)
-                        x = y
-			tx = time.time()
-def print_ip():
-	lcd.clrscr()
-	lcd.println("System Ready")
-	lcd.println(" ")
-	lcd.println("Fetching IP")
-	ip = str(subprocess.check_output("hostname -I", shell=True)).strip()
-	t = time.time()
-	while len(ip)<1:
-		tx = time.time()
-		if tx-t>=0.5:
-			ip = str(subprocess.check_output("hostname -I", shell=True)).strip()
-def get_prn(id):
-    map = json.load(open("./docs/map.json"))
-    prn = map[str(id)]
-    return prn
-def light_show():
-	LedPin1 = 11
-	LedPin2 = 12
-        fade = 20
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setwarnings(False)
-	GPIO.setup(LedPin1, GPIO.OUT)
-	GPIO.setup(LedPin2, GPIO.OUT)
-	fps.setLED(True)
-	while not fps.isPressFinger():
-                GPIO.output(LedPin2,True)
-                GPIO.output(LedPin1,False)
-                time.sleep(0.5)
-                GPIO.output(LedPin1,True)
-                GPIO.output(LedPin2,False)
-                time.sleep(0.5)
-        GPIO.output(LedPin1,False)
-        GPIO.output(LedPin2,False)
-lcd.clrscr()
-lcd.println("System Ready")
-lcd.println("Initialize FPS")
-fps.open()
-#print_time()
+println("Boot Complete")
+println("Getting Ready")
+setup()
+enroll(5)
 while True:
-    lcd.clrscr();
-    lcd.println("Press Finger")
+    clrscr()
+    println("Press Finger")
     id = fps.identify()
     if int(id)==200:
-	lcd.clrscr()
-        lcd.println("Finger not found")
+	clrscr()
+        println("Finger not found")
 	#lcd.println("Light Show")
 	#beep(2)
 	time.sleep(1)
 	#light_show()
     else:
         prn = get_prn(id)
-        lcd.clrscr()
-        lcd.println("PRN:"+str(prn))
-        lcd.println("Wait 1 Second")
+        clrscr()
+        println("PRN:"+str(prn))
+        println("Wait 1 Second")
         time.sleep(1)
--- functions.py
+++ functions.py
@@ -10,6 +10,107 @@ user = "root"
 password = "cerberus"
 database = "cerberus"
 labid = 1
+def setup():
+        while not fps.open:
+                fps.open()
+def enroll(id):
+        fps.enroll(id)
+        clrscr()
+        iret = 0
+        errCount = 0
+        while errCount <= 2:
+                if (errCount > 0
+                        println("Press Finger")
+                        fps.waitForFinger()
+                bret = fps.captureFinger(True)
+                if not bret
+                        errCount = 0
+                        fps.enroll1()
+                        clrscr()
+                        println("Captured Image 1/3")
+                        println("Remove Finger")
+                        fps.waitForFinger()
+                        while errCount <= 2:
+                                clrscr()
+                                println("Press Same Finger")
+                                fps.waitForFinger()
+                                println("Reading Finger")
+                                bret = fps.CaptureFinger(True)
+                                if not bret:
+                                        errCount = 0
+                                        fps.enroll2()
+                                        clrscr()
+                                        println("Captured Image 2/3")
+                                        println("Remove finger")
+                                        fps.waitForFinger()
+                                        while errCount <= 2:
+                                                clrscr()
+                                                println("Press Same Finger")
+                                                fps.waitForFinger()
+                                                println("Reading Finger")
+                                                bret = fps.CaptureFinger(True)
+                                                if not bret:
+                                                        errCount = 0
+                                                        iret = fps.enroll3()
+                                                        clrscr()
+                                                        println("Captured Image 3/3")
+                                                        println("Remove finger")
+                                                        fps.waitForFinger()
+                                                        clrscr()
+                                                        if iret == 0:
+                                                                println("Enroll Successfull")
+                                                                errCount = 3
+                                                                time.sleep(1)
+                                                        else
+                                                                clrscr()
+                                                                println("Error code :" + String(iret))
+                                                                switch (iret)
+                                                                        case 1 : println("Error")
+                                                                        break
+                                                                        case 2 : println("Bad Finger")
+                                                                        break
+                                                                        case 3 : println("Duplicate Finger")
+                                                                        break
+                                                                time.sleep(5)
+                                                                errCount = 4
+                        
+            
+                                                else
+                                                        clrscr()
+                                                        println("Failed - 3rd Image")
+                                                        println("Wet/Dry/Dirty Finger")
+                                                        println("Cannot be Imaged")
+                                                        println("Please Try Again")
+                                                        errCount++
+                                                        time.sleep(3)
+                                else
+                                        clrscr()
+                                        println("Failed - 2nd Image")
+                                        println("Wet/Dry/Dirty Finger")
+                                        println("Cannot be Imaged")
+                                        println("Please Try Again")
+                                        errCount++
+                                        time.sleep(3)
+                                        clrscr()
+                                        println("Press Finger Agian")
+                else
+                        clrscr()
+                        println("Failed - 1st Image")
+                        println("Wet/Dry/Dirty Finger")
+                        println("Cannot be Imaged")
+                        println("Please Try Again")
+                        errCount++
+                        time.sleep(3)
+        if not errCount == 3:
+                clrscr()
+                println("Enrollment Failed")
+                println("Image Capture Failed")
+                println("Inform Lab Teacher")
+                time.sleep(5)
+def clrscr():
+        lcd.clrscr()
+def println(text):
+        lcd.println(text)
 def beep(sec):
         BuzzPin =36
         GPIO.setmode(GPIO.BOARD)
@@ -147,7 +248,7 @@ def sync_attendance():
                     print(format(err))
 	    else:
  	        try:
-   	            sql="UPDATE `timetable` SET `facultyID`=%s WHERE scheduleid=%s"
+   	            sql="UPDATE `facultytimetable` SET `facultyID`=%s WHERE scheduleid=%s"
 		    val = (attendance[2],scheduleid)
  	   	    cur.execute(sql,val)
                 except mysql.connector.Error as err:
@@ -222,7 +323,7 @@ def print_enrolled():
     t = rtc.getTime()
     lcd.println(t)
     count = fps.countEnrolled()
-    i = 0;
+    i = 0
     print 'Total number of enrolled fingerprints = '+str(count)
     found=0
     while (found<count):
@@ -230,26 +331,36 @@ def print_enrolled():
  		print 'Fingerprint Count '+str(found)+' is at ID '+str(i)
 		found = found+1
 	i=i+1
+def get_prn(id):
+        map = json.load(open("./docs/map.json"))
+        prn = map[str(id)]
+        return prn
+def light_show():
+	LedPin1 = 11
+	LedPin2 = 12
+        fade = 20
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setwarnings(False)
+	GPIO.setup(LedPin1, GPIO.OUT)
+	GPIO.setup(LedPin2, GPIO.OUT)
+	fps.setLED(True)
+	while not fps.isPressFinger():
+                GPIO.output(LedPin2,True)
+                GPIO.output(LedPin1,False)
+                time.sleep(0.5)
+                GPIO.output(LedPin1,True)
+                GPIO.output(LedPin2,False)
+                time.sleep(0.5)
+        GPIO.output(LedPin1,False)
+        GPIO.output(LedPin2,False)
+def print_time(line):
+        t = time.time()
+        lcd.println(str(rtc.hour)+':'+str(rtc.min)+':'+str(rtc.sec)+' '+str(rtc.date)+'/'+str(rtc.month)+'/'+str(rtc.year),3)
 
-def print_time():
-	ip = str(subprocess.check_output("hostname -I", shell=True)).strip()
-	lcd.clrscr()
-	lcd.println("System Ready")
-	lcd.println(ip)
-	lcd.println("Time :")
-	x = rtc.getTime()
-	t = time.time()
-	print 'Printing time on LCD'
-	print 'Press Ctrl + C to Abort'
-	while True:
-		y = rtc.getTime()
-		if x!=y:
-			lcd.println(str(rtc.hour)+':'+str(rtc.min)+':'+str(rtc.sec)+' '+str(rtc.date)+'/'+str(rtc.month)+'/'+str(rtc.year),4)
-			x = y
 def set_template(template_name,fps_id):
     text = open("./templates/"+str(template_name)+".txt","rb") 
     template_data = text.readlines() 
     text.close()
-    response = fps.setTemplate(id,str(template_data));
+    response = fps.setTemplate(id,str(template_data))
     print response
     print 'Templates written to fps successfully'
